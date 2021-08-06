e388ab01bc677ce316a0142c30b3559ac125658e
n3tman <n3tman@users.noreply.github.com>
2021-08-06 20:37:06 +0300

    Add upvote/downvote icons to levels list, replace favoritesBadge

---
+++ BeatSaverVoting/HarmonyPatches/LevelSelectionPatch.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BeatSaverVoting.HarmonyPatches
+{
+    [HarmonyPatch(typeof(LevelListTableCell))]
+    [HarmonyPatch("SetDataFromLevelAsync", MethodType.Normal)]
+    internal class LevelListTableCellSetDataFromLevel
+    {
+        private static void Postfix(IPreviewBeatmapLevel level, bool isFavorite, ref UnityEngine.UI.Image ____favoritesBadgeImage)
+        {
+            if (!(level is CustomPreviewBeatmapLevel customLevel))
+            {
+                return;
+            }
+
+            var hash = SongCore.Collections.hashForLevelID(level.levelID).ToLower();
+            var voteStatus = Plugin.CurrentVoteStatus(hash);
+
+            if (voteStatus == null && !isFavorite)
+            {
+                return;
+            }
+
+            ____favoritesBadgeImage.enabled = true;
+
+            if (isFavorite)
+            {
+                switch (voteStatus)
+                {
+                    case Plugin.VoteType.Upvote: { ____favoritesBadgeImage.sprite = Plugin.FavoriteUpvoteIcon; break; }
+                    case Plugin.VoteType.Downvote: { ____favoritesBadgeImage.sprite = Plugin.FavoriteDownvoteIcon; break; }
+                    case null: { ____favoritesBadgeImage.sprite = Plugin.FavoriteIcon; break; }
+                }
+            }
+            else
+            {
+                switch (voteStatus)
+                {
+                    case Plugin.VoteType.Upvote: { ____favoritesBadgeImage.sprite = Plugin.UpvoteIcon; break; }
+                    case Plugin.VoteType.Downvote: { ____favoritesBadgeImage.sprite = Plugin.DownvoteIcon; break; }
+                }
+            }
+
+            ____favoritesBadgeImage.rectTransform.sizeDelta = new Vector2(3.5f, 7);
+            var transform = ____favoritesBadgeImage.transform;
+            var position = transform.position;
+            position = new Vector3(-0.47f, position.y, position.z);
+            transform.position = position;
+        }
+    }
+}
